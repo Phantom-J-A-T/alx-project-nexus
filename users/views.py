0c85f78ec1f600d0f9d8584124f34f74e39b99ac
Phantom-J-A-T <ramonadedotun550@gmail.com>
2026-02-08 21:24:38 +0100

    feat: Implement `IsOwnerOrAdmin` permission for `UserDetailView` to restrict access to user details.

--- users/views.py
+++ users/views.py
@@ -1,24 +1,27 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import UserSerializer
+from .permissions import IsOwnerOrAdmin # Import your new permission
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-# GET (all) and POST
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
-    # Allow registration (POST), but maybe require login to see the list (GET)
+
     def get_permissions(self):
+        # Allow anyone to POST (register)
         if self.request.method == 'POST':
             return [AllowAny()]
+        # Require login to see the list of all users
         return [IsAuthenticated()]
 
-# GET (one), PUT/PATCH, and DELETE
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
     lookup_field = 'id'
+    
+    # Use the custom permission: 
+    # Must be logged in AND (must be the owner OR an admin)
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
