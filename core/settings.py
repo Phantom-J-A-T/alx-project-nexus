847e15a55e1f6a2c4c88ec97ecc546a704b126e2
Phantom-J-A-T <ramonadedotun550@gmail.com>
2026-02-08 22:45:37 +0100

    Updating token permissions

--- core/settings.py
+++ core/settings.py
@@ -68,6 +68,8 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
 }
 
